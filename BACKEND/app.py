20fe02a825cb57d35016f26073183ee7eb208674
Hassan Jamshaid <hassanjamshaid364@gmail.com>
2025-01-08 13:47:06 +0500

    db changes required

--- BACKEND/app.py
+++ BACKEND/app.py
@@ -288,8 +288,8 @@ async def mark_attendance(
                 # Step 2: Insert the data into the database (only if no existing entry found)
                 insert_query = """
                     INSERT INTO employee_management_temp_appattendance 
-                    (employee_id, time, date, log_type, x_coordinate, y_coordinate, location_address, status)
-                    VALUES (%s, %s, %s, %s, %s, %s, %s, 'Pending')
+                    (employee_id, time, date, log_type, x_coordinate, y_coordinate, location_address, status, image)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, 'Pending', %s)
                 """
                 cursor.execute(insert_query, (
                     employee_id,
@@ -298,8 +298,10 @@ async def mark_attendance(
                     log_type,
                     x,
                     y,
-                    address
+                    address,
+                    image_bytes  # Insert the binary image data
                 ))
+
 
                 # Commit the transaction
                 mydb.commit()
